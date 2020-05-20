2b1bcaf5c0c60741dd0b3e9e750cca176d2e1085
Frank Denis <github@pureftpd.org>
2020-05-20 17:47:48 +0200

    Derive std::error::Error

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,20 @@
+use std::fmt::{self, Display};
+
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub enum Error {
     /// The provided output buffer would be too small.
     Overflow,
     /// The input isn't valid for the given encoding.
     InvalidInput,
+}
+
+impl std::error::Error for Error {}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Error::Overflow => write!(f, "Overflow"),
+            Error::InvalidInput => write!(f, "Invalid input"),
+        }
+    }
 }
